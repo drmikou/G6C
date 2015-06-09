52a0aa5fda3c115f5552dc58b51eb81fe943d023
drmikou <phanthavong.mickael@gmail.com>
2015-06-09 09:53:08 +0200

    Affichage des étoiles

---
+++ vue/notation_affichage.php
@@ -0,0 +1,40 @@
+if(float round( float $data['user_rate'] [, int $precision =0[, int $mode= PHP_ROUND_HALF_UP]] == 1)
+{
+  for($i=1; $i<=1;i++)
+  {
+    <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
+  }
+}
+
+if(float round( float $data['user_rate'] [, int $precision =0[, int $mode= PHP_ROUND_HALF_UP]] == 2)
+{
+  for($i=1; $i<=2;i++)
+  {
+    <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
+  }
+}
+
+if(float round( float $data['user_rate'] [, int $precision =0[, int $mode= PHP_ROUND_HALF_UP]] == 3)
+{
+  for($i=1; $i<=3;i++)
+  {
+    <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
+  }
+}
+
+if(float round( float $data['user_rate'] [, int $precision =0[, int $mode= PHP_ROUND_HALF_UP]] == 4)
+{
+  for($i=1; $i<=4;i++)
+  {
+    <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
+  }
+}
+
+if(float round( float $data['user_rate'] [, int $precision =0[, int $mode= PHP_ROUND_HALF_UP]] == 5)
+{
+  for($i=1; $i<=5;i++)
+  {
+    <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
+  }
+}
+
