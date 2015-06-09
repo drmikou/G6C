59f9ed5c675df439b37e1b1f209af569ca2b8534
Sarpoon <aurelienduval33@yahoo.fr>
2015-06-09 23:09:26 +0200

    Update actualite_ajouter.php

--- modele/actualite_ajouter.php
+++ modele/actualite_ajouter.php
@@ -6,7 +6,7 @@
 			$contenu= $_POST['contenu'];
 		
 
-		$query=$db->prepare('INSERT INTO news (news_title, news_content) VALUES (:titre, :contenu)');
+		$query=$db->prepare('INSERT INTO news (news_title, news_content, news_date) VALUES (:titre, :contenu, NOW())');
 		$query->bindValue(':titre', $titre, PDO::PARAM_STR);
 		$query->bindValue(':contenu', $contenu, PDO::PARAM_STR);
 		$query->execute();
