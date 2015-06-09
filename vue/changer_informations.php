847fae90de1496e513409936a70ee3af3e78ef47
Sarpoon <aurelienduval33@yahoo.fr>
2015-06-09 23:57:06 +0200

    Update changer_informations.php

--- vue/changer_informations.php
+++ vue/changer_informations.php
@@ -14,18 +14,21 @@
             $mdp_new= $_POST['mdp_new'];
             $mdp_new_check= $_POST['mdp_new_check'];
 
+			$key = 'f6d2qds64q92';
+			$mdp_actuel_encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256,md5($key), $mdp_actuel,MCRYPT_MODE_CBC, md5(md5($key))));
 
             $query = $db->prepare('SELECT user_password FROM user WHERE user_login= :login'); //PDO::prepare — Prépare une requête à l'exécution et retourne un objet
             $query->execute(array('login' => $pseudo)); // Exécute la préparation
             $data = $query->fetch();
 
-            if($data['user_password'] == $mdp_actuel && $mdp_new == $mdp_new_check && 3 < strlen($mdp_new) && strlen($mdp_new) < 15)
+            if($data['user_password'] == $mdp_actuel_encrypted && $mdp_new == $mdp_new_check && 3 < strlen($mdp_new) && strlen($mdp_new) < 15)
             {
+            	$mdp_new_encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256,md5($key), $mdp_new,MCRYPT_MODE_CBC, md5(md5($key))));
                 $query= $db->prepare('UPDATE user SET user_password = :mdp WHERE user_login= :login');
-                $query->execute(array('mdp'=> $mdp_new ,'login'=> $pseudo)); // Exécute la préparation
+                $query->execute(array('mdp'=> $mdp_new_encrypted ,'login'=> $pseudo)); // Exécute la préparation
                 echo "Votre mot de passe a bien été changé!";
             }
-            elseif($data['user_password'] != $mdp_actuel)
+            elseif($data['user_password'] != $mdp_actuel_encrypted)
             {
                 echo "Votre mot de passe est faux!";
             }
@@ -54,17 +57,20 @@
             $mail_new_check= $_POST['mail_new_check'];
 
 
+			$key = 'f6d2qds64q92';
+			$mdp_actuel_mail_encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256,md5($key), $mdp_actuel_mail,MCRYPT_MODE_CBC, md5(md5($key))));
+			
             $query = $db->prepare('SELECT user_password FROM user WHERE user_login= :login'); //PDO::prepare — Prépare une requête à l'exécution et retourne un objet
             $query->execute(array('login' => $pseudo)); // Exécute la préparation
             $data = $query->fetch();
 
-            if($data['user_password'] == $mdp_actuel_mail && $mail_new == $mail_new_check)
+            if($data['user_password'] == $mdp_actuel_mail_encrypted && $mail_new == $mail_new_check)
             {
                 $query= $db->prepare('UPDATE user SET user_mail = :mail WHERE user_login= :login');
                 $query->execute(array('mail'=> $mail_new ,'login'=> $pseudo)); // Exécute la préparation
                 echo "Votre mail a bien été changé!";
             }
-            elseif($data['user_password'] != $mdp_actuel_mail)
+            elseif($data['user_password'] != $mdp_actuel_mail_encrypted)
             {
                 echo "Votre mot de passe est faux!";
             }
