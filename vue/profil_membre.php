aab2540ecc5c9df900a933a80a221195977f5a32
drmikou <phanthavong.mickael@gmail.com>
2015-06-09 10:46:20 +0200

    avec isset

--- vue/profil_membre.php
+++ vue/profil_membre.php
@@ -12,19 +12,27 @@
             <div id="profil_data">
 
                 <div class="titre"> Profil recherché</h2> </div>
+                        if(isset($data['user_login']))
+                        {
+                            <div class="bloc">
+                                <img  src="../vue/images/profil.png" class="imageflottante" alt="profil" id="profil" width="30" /><div class= "decalage">Pseudo :</div> <br>     <div class="donnee"> <?php echo $data['user_login'] ?> </div> <br>
+                            </div>
+                        }
+                        
+                        if(isset($data['user_surname']))
+                        {
+                            <div class="bloc">
+                                <img  src="../vue/images/profil.png" class="imageflottante" alt="profil" id="profil" width="30" /><div class= "decalage">Prénom :</div> <br>     <div class="donnee"> <?php echo $data['user_surname'] ?> </div> <br>
+                            </div>
+                        }
 
-                    <div class="bloc">
-                        <img  src="../vue/images/profil.png" class="imageflottante" alt="profil" id="profil" width="30" /><div class= "decalage">Pseudo :</div> <br>     <div class="donnee"> <?php echo $data['user_login'] ?> </div> <br>
-                    </div>
-
-                    <div class="bloc">
-                        <img  src="../vue/images/profil.png" class="imageflottante" alt="profil" id="profil" width="30" /><div class= "decalage">Prénom :</div> <br>     <div class="donnee"> <?php echo $data['user_surname'] ?> </div> <br>
-                    </div>
-
-                    <div class="bloc">
-                        <img  src="../vue/images/region.png" class="imageflottante" alt="region" id="region" width="30" /><div class= "decalage">Adresse : </div><br> <br>     <div class="donnee"> <?php echo $data['user_town'] ?> <?php echo $data['user_region'] ?> </div> <br>
-                    </div>
-
+                        if(isset($data['user_town']))
+                        {
+                            <div class="bloc">
+                                <img  src="../vue/images/region.png" class="imageflottante" alt="region" id="region" width="30" /><div class= "decalage">Adresse : </div><br> <br>     <div class="donnee"> <?php echo $data['user_town'] ?> <?php echo $data['user_region'] ?> </div> <br>
+                            </div>
+                        }
+                        
                     <div class="bloc">
                         <img  src="../vue/images/note.png" class="imageflottante" alt="region" id="region" width="30" />
                                 <div class= "decalage">Note : </div><br> <br>     
