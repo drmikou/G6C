0f3fb998176c4423b0a0689c8bff5b6193c59f27
Sarpoon <aurelienduval33@yahoo.fr>
2015-06-11 10:29:03 +0200

    Update profil_supprimer_check.php

--- modele/profil_supprimer_check.php
+++ modele/profil_supprimer_check.php
@@ -1,6 +1,8 @@
 <?php
 
 	$mdp_actuel= $_POST['mdp_actuel'];
+	$key = 'f6d2qds64q92';
+	$mdp_actuel_encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256,md5($key), $mdp_actuel,MCRYPT_MODE_CBC, md5(md5($key))));
 		
 	$query = $db->prepare('SELECT user_password FROM user WHERE user_login= :login'); //PDO::prepare — Prépare une requête à l'exécution et retourne un objet
 	$query->execute(array('login' => $pseudo)); // Exécute la préparation
